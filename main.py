0abf5a3335143efb2671ff6b4d1b4e835e556e94
uniq47 <unique.sapkota01@student.csulb.edu>
2023-02-19 19:17:53 -0800

    git added code insdie the fucnion for curling html

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import urllib.request
 
-response = urllib.request.urlopen(
-    "http://en.wikipedia.org/wiki/List_of_HTTP_status_codes")
-print(response)
-print(dir(response))
-print(response.status)
-responsetext = response.read()
-print(len(responsetext))
-print(type(responsetext))
-with open('status.html', 'wb') as filepointer:
-    filepointer.write(responsetext)
+
+def downlaod(url, FILE):
+
+    response = urllib.request.urlopen(url)
+    print(response.status)
+    responsetext = response.read()
+    with open('status.html', 'wb') as fp:
+    fp.write(responsetext)
+
+    return responsetext
+
+
+# response = urllib.request.urlopen(
+#     "http://en.wikipedia.org/wiki/List_of_HTTP_status_codes")
+# print(response)
+# print(dir(response))
+# print(response.status)
+#  with open('status.html', 'wb') as filepointer:
+#     filepointer.write(responsetext)
+# print(len(responsetext))
+# print(type(responsetext))
+
+
+page = download(
+    url="http://en.wikipedia.org/wiki/List_of_HTTP_status_codes"
+    FILE="status.html")
