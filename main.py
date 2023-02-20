9c88c09c66362c7f468e13f0fe537f87ef81d1bf
uniq47 <unique.sapkota01@student.csulb.edu>
2023-02-19 19:07:30 -0800

    getting html form the weki page

--- main.py
+++ main.py
@@ -1,35 +1,12 @@
-#form selenium import webdriver
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import urllib.request
 
-PATH ="C:\Program Files (x86)\chromedriver.exe"
-driver=webdriver.Chrome(PATH)
-
-
-driver.get("https://www.techwithtim.net/")
-print(driver.title)
-
-search = driver.find_element_by_name("s")
-search.send_keys("test")
-search.send_keys(Keys.RETURN)
-
-try:
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "main"))
-    )
-    articles=main.find_elements_by_tag_name("article")
-    for article in articles:
-        header= article.find_element_by_class_name("entry-summary")
-        print(header.text)
-
-finally:
-    driver.quit()
-
-
-print(main.text)
-
-# driver.close()
+response = urllib.request.urlopen(
+    "http://en.wikipedia.org/wiki/List_of_HTTP_status_codes")
+print(response)
+print(dir(response))
+print(response.status)
+responsetext = response.read()
+print(len(responsetext))
+print(type(responsetext))
+with open('status.html', 'wb') as filepointer:
+    filepointer.write(responsetext)
